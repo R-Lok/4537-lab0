6f82d369b310a03cbff5abc7fc3449493ecdd3c8
Ryan Lok <rlok.pc@gmail.com>
2025-01-08 21:00:39 -0800

    Implemented shuffling

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
 let game;
 class Game {
     constructor(numButtons) {
+        this.numButtons = numButtons
         this.buttons = []
         this.container = document.createElement('div')
         this.container.style.width = '100%'
         this.container.style.height = '90%'
+        this.container.style.position = 'relative'
         document.body.appendChild(this.container)
     }
 
-    createButtons(numButtons) {
-        for(let i = 0; i < numButtons; i++) {
-            this.buttons.push(new Button(i))
+    createButtons() {
+        for (let i = 0; i < this.numButtons; i++) {
+            this.buttons.push(new Button(i + 1))
             this.container.appendChild(this.buttons[i].btn);
+        }
+    }
+
+    startGame() {
+        for (let i = 0; i < this.numButtons; i++) {
+            this.buttons[i].moveButton(this.container.offsetHeight, this.container.offsetWidth)
         }
     }
 
@@ -24,10 +32,37 @@ class Button {
     constructor(number) {
         this.btn = document.createElement('button')
         this.btn.innerText = number
+        this.btn.id = number
         this.btn.style.height = '5em'
         this.btn.style.width = '10em'
         this.btn.style.margin = '0.5em'
         this.btn.style.backgroundColor = selectRandomColor();
+        this.position = new Position()
+    }
+
+    moveButton(containerHeight, containerWidth) {
+        this.btn.style.position = 'absolute'
+        this.position.randomizePosition(this.btn.offsetHeight, this.btn.offsetWidth, containerHeight, containerWidth)
+        this.btn.style.top = this.position.top
+        this.btn.style.left = this.position.left
+    }
+}
+
+class Position {
+    constructor() {
+        this.top = "0px"
+        this.left = "0px"
+    }
+
+    randomizePosition(offsety, offsetx, containerHeight, containerWidth) {
+
+        // Convert the button's dimensions from pixels to percentages of the container
+        const maxTop = containerHeight - parseFloat(offsety);
+        const maxLeft = containerWidth - parseFloat(offsetx);
+
+        // Now randomize the position within these bounds
+        this.top = `${Math.random() * maxTop}px`;
+        this.left = `${Math.random() * maxLeft}px`;
     }
 }
 
@@ -42,16 +77,17 @@ document.getElementById('go-button').addEventListener('click', e => {
     e.preventDefault()
     const inputValue = parseInt(document.getElementById('num-buttons-input').value)
 
-    if(!checkValidInput(inputValue)) {
+    if (!checkValidInput(inputValue)) {
         alert(inputWarning)
         return
     }
-    if(game)
+    if (game)
         game.endSession()
-    game = new Game()
-    game.createButtons(inputValue)
+    game = new Game(inputValue)
+    game.createButtons()
+    game.startGame()
 })
 
-function checkValidInput(inputValue){
+function checkValidInput(inputValue) {
     return inputValue >= 3 && inputValue <= 7
 }
