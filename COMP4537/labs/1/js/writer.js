69d0d1f3517ca80dfc9eb441ef180fd934efcb75
Ryan Lok <rlok.pc@gmail.com>
2025-01-16 22:25:08 -0800

    Implemented basic notes storage and loading

--- COMP4537/labs/1/js/writer.js
+++ COMP4537/labs/1/js/writer.js
@@ -1,4 +1,69 @@
+
+class Note {
+    constructor(string = "") {
+        this.text = string
+    }
+
+    setText(string) {
+        this.text = string
+    }
+}
+class NoteElement {
+    constructor(note) {
+        this.note = note
+
+        const container = document.createElement ("div")
+        this.UIelement = container
+
+        const textArea = document.createElement("textarea")
+        const btn = document.createElement("button")
+
+        container.classList.add("editable-note")
+        container.appendChild(textArea)
+        container.appendChild(btn)
+
+        textArea.value = note.text
+        btn.textContent = writerRemoveButton
+
+        document.getElementById("notes-container").append(container)
+        storeToLocalStorage()
+    }
+}
+
+let notes = [];
 (function(){
     //inject strings into the page
     document.getElementById("add-button").innerText = writerAddButton
+
+    //immediately retrieve data from localStorage
+    retrieveFromLocalStorage()
+
+    if(notes.length != 0) {
+        const container = document.getElementById("notes-container")
+        for(let i = 0; i < notes.length; i++) {
+            const nElement = new NoteElement(notes[i])
+            container.appendChild(nElement.UIelement)
+        }
+    }
 })()
+
+document.getElementById("add-button").addEventListener("click", (e) => {
+    const note = new Note()
+    notes.push(note)
+    new NoteElement(note)
+})
+
+function storeToLocalStorage() {
+    const stringifiedArr = JSON.stringify(notes)
+    localStorage.setItem("data", stringifiedArr)
+}
+
+function retrieveFromLocalStorage() {
+    const stringifiedArr = localStorage.getItem("data")
+    if (stringifiedArr) {
+        const parsedNotes = JSON.parse(stringifiedArr)
+        notes = parsedNotes.map(obj => {
+            return new Note(obj.text)
+        })
+    }
+}
--- COMP4537/labs/1/lang/messages/en/user.js
+++ COMP4537/labs/1/lang/messages/en/user.js
@@ -1,4 +1,7 @@
 let indexTitle = "Lab 1: JSON, Object Constructor, localStorage, Ryan Lok"
 let indexWriterButton = "Go to writer"
 let indexReaderButton = "Go to reader"
+
+
 let writerAddButton = "Add"
+let writerRemoveButton = "Remove"
